587c6c3bf01999a67e94a0c05ba6a68332c050b1
Jeffrey Way <jeffrey@envato.com>
2013-05-04 01:39:22 -0400

    Add assertRespondsTo

--- src/Way/Tests/ModelHelpers.php
+++ src/Way/Tests/ModelHelpers.php
@@ -11,13 +11,13 @@ trait ModelHelpers {
 
     public function assertValid($model)
     {
-        $this->assertHasMethod('validate', $model, "The 'validate' method does not exist on this model.");
+        $this->assertRespondsTo('validate', $model, "The 'validate' method does not exist on this model.");
         $this->assertTrue($model->validate(), 'Model did not pass validation.');
     }
 
     public function assertNotValid($model)
     {
-        $this->assertHasMethod('validate', $model, "The 'validate' method does not exist on this model.");
+        $this->assertRespondsTo('validate', $model, "The 'validate' method does not exist on this model.");
         $this->assertFalse($model->validate(), 'Did not expect model to pass validation.');
     }
 
@@ -36,7 +36,7 @@ trait ModelHelpers {
         $this->assertRelationship($relation, $class, 'hasOne');
     }
 
-    public function assertHasMethod($method, $class, $message = null)
+    public function assertRespondsTo($method, $class, $message = null)
     {
         $message = $message ?: "Expected the '$class' class to have method, '$method'.";
 
@@ -48,7 +48,7 @@ trait ModelHelpers {
 
     public function assertRelationship($relationship, $class, $type)
     {
-        $this->assertHasMethod($relationship, $class);
+        $this->assertRespondsTo($relationship, $class);
 
         $class = Mockery::mock($class."[$type]");
 
